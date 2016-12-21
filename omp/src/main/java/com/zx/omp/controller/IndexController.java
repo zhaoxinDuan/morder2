5e5070c4e1665d1eb4754e1a545db31497b7e321
simba <simba@dacrs.com>
2016-12-21 17:57:20 +0800

    init

--- omp/src/main/java/com/zx/omp/controller/IndexController.java
+++ omp/src/main/java/com/zx/omp/controller/IndexController.java
@@ -10,7 +10,72 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class IndexController {
     @RequestMapping("/")
-    public String index() {
+    public String indexpage() {
         return "/login";
+    }
+
+    @RequestMapping("/login")
+    public String login() {
+        return "/login";
+    }
+
+    @RequestMapping("/index")
+    public String index() {
+        return "/index";
+    }
+
+    @RequestMapping("/flot")
+    public String flot() {
+        return "/flot";
+    }
+
+    @RequestMapping("/morris")
+    public String morris() {
+        return "/morris";
+    }
+
+    @RequestMapping("/tables")
+    public String tables() {
+        return "/tables";
+    }
+
+    @RequestMapping("/forms")
+    public String forms() {
+        return "/forms";
+    }
+
+    @RequestMapping("/panels-wells")
+    public String panelsWells() {
+        return "/panels-wells";
+    }
+
+    @RequestMapping("/buttons")
+    public String buttons() {
+        return "/buttons";
+    }
+
+    @RequestMapping("/notifications")
+    public String notifications() {
+        return "/notifications";
+    }
+
+    @RequestMapping("/typography")
+    public String typography() {
+        return "/typography";
+    }
+
+    @RequestMapping("/icons")
+    public String icons() {
+        return "/icons";
+    }
+
+    @RequestMapping("/grid")
+    public String grid() {
+        return "/grid";
+    }
+
+    @RequestMapping("/blank")
+    public String blank() {
+        return "/blank";
     }
 }
